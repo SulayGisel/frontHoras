87882081986ef4e2c60d05a9ca904390800ad6b0
SulayGisel <sulaymartinez939@gmail.com>
2025-05-05 08:29:27 -0500

    despliegue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    url:'http://localhost:3000/api/v1/',
-    production: false
+   // url:'http://localhost:3000/api/v1/',
+   url:'https://backhorasextra-production.up.railway.app/api/v1/',
+    production: true
   };
